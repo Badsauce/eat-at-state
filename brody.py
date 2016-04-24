c0c6095b7711687d04877c476a0bd660c4597a2d
Nick Rutowski <ninjazoby@gmail.com>
2016-04-24 00:34:04 -0400

    prints a list of all menu items, unaddressed edge cases

--- brody.py
+++ brody.py
@@ -1,8 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
+dish_list = []
+
 with urllib.request.urlopen('https://eatatstate.com/menus/brody') as response:
    html = response.read()
    soup = BeautifulSoup(html, 'html.parser')
+   menuItems = soup.find_all(class_="field-item")
+   for item in menuItems:
+       dish_list.append(str(item.string))
 
-   print(soup.prettify())
+   print(dish_list)
